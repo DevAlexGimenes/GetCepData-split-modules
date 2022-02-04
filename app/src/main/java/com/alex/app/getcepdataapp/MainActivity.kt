fdfa81948aeea466b8ab72e96c0e7b4d8a5a81c5
Alex Gimenes <heydev2001@gmail.com>
2022-02-04 08:17:54 -0300

    Core splash screen

--- app/src/main/java/com/alex/app/getcepdataapp/MainActivity.kt
+++ app/src/main/java/com/alex/app/getcepdataapp/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.alex.app.getcepdataapp
 
 import android.os.Bundle
+import android.view.View
+import android.view.ViewTreeObserver
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.alex.app.getcepdataapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        installSplashScreen()
+        setContentView(view)
         supportActionBar?.hide()
+
+        val content: View = findViewById(android.R.id.content)
+        content.viewTreeObserver.addOnPreDrawListener(
+            object : ViewTreeObserver.OnPreDrawListener {
+                val delay = 2000L
+                override fun onPreDraw(): Boolean {
+                    Thread.sleep(delay)
+                    content.viewTreeObserver.removeOnPreDrawListener(this)
+                    return true
+                }
+            }
+        )
     }
 }
